8431fde6eae94ba6ec2f6b268b13bc5703e2cd4c
Federico Ramallo <framallo@gmail.com>
2008-11-21 13:53:08 -0200

    added smart defaults

---
+++ emacsonrails-smart-defaults.el
@@ -0,0 +1,39 @@
+; this is a list of smart defaults 
+; we've been using to share some customizations
+; while we configure emacs
+;
+; You could add it or use those you like
+
+;disabes the emacs splash-screen
+(setq inhibit-splash-screen t)
+
+;enables clipboard interaction with the X window system
+(setq x-select-enable-clipboard t)
+
+;hides the menu bar
+(menu-bar-mode 0)
+
+;hides the toolbar
+(tool-bar-mode 0)
+
+; reduce font size using: "7x14" 
+; Font Menu: Shift-Mouse1
+(set-default-font "Monospace-9")
+(modify-all-frames-parameters
+'((font . "Monospace-9")))
+
+; run color-theme friendly with rails mode
+; the only issue is to make the errors more legible with black background 
+(color-theme-initialize )
+(color-theme-goldenrod )
+
+;improves on buffer switch
+; C-s next 
+; C-r previous
+(iswitchb-mode)
+
+; create buffers like file & file<folder>
+(uniquify-buffer-name-style (quote post-forward-angle-brackets) nil (uniquify))
+
+
+(server-start)
