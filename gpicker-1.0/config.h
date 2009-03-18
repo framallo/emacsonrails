a218b172d8be76ab9b331b7dbe9299318fe420e7
Federico Ramallo <framallo@gmail.com>
2009-03-18 10:58:07 -0600

    modified color-theme and added gpicker

---
+++ gpicker-1.0/config.h
@@ -0,0 +1,26 @@
+/* config.h.  Generated from config.h.in by configure.  */
+/* config.h.in.  Generated from configure.ac by autoheader.  */
+
+/* Name of package */
+#define PACKAGE "gpicker"
+
+/* Define to the address where bug reports for this package should be sent. */
+#define PACKAGE_BUGREPORT ""
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME "gpicker"
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING "gpicker 1.0"
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME "gpicker"
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "1.0"
+
+/* Version number of package */
+#define VERSION "1.0"
+
+/* Output timing info */
+/* #undef WITH_TIMING */
